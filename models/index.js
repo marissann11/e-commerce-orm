d5dfee415ac257087859ceefdfc703e714e28f64
marissann11 <wilhite.marissa@gmail.com>
2022-02-16 17:00:16 -0500

    added CRUD to category routes

--- models/index.js
+++ models/index.js
@@ -1,34 +1,30 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
-//category can have multiple products 
-//but a product can only belong to one category.
 Product.belongsTo(Category, {
-  foreignKey: 'category_id'
+  foreignKey: "category_id",
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'category_id'
+  foreignKey: "category_id",
 });
 
 // Products belongToMany Tags (through ProductTag)
-//Using the ProductTag through model
-//allow products to have multiple tags and tags to have many products.
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  foreignKey: "product_id"
+  foreignKey: "product_id",
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  foreignKey: "tag_id"
-})
+  foreignKey: "tag_id",
+});
 
 module.exports = {
   Product,
